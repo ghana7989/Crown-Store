c1aab8ac2b8f32b0131e4aa35a356d9f7d1d7159
ghana7989 <ghana.chindukuri@gmail.com>
2020-07-29 13:47:26 +0530

    adding some css to HomePage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from './homepage.components';
+import HomePage from './pages/Homepage/homepage.components';
 
 class App extends Component {
   render() {
