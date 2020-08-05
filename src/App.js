0c562c8b6c7367200a831b62ae9ed974bd88bbb9
ghana7989 <ghana.chindukuri@gmail.com>
2020-08-05 14:21:35 +0530

    Upadting app with collection preview component and improving Shop page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/Homepage/homepage.components';
+import ShopPage from './pages/shop/shop.components.jsx'
 
 
-const HatsPage = () => (
-  <div>
-    <h1> HATSSSSS </h1>
-  </div>
-)
+
 
 
 class App extends Component {
@@ -16,7 +13,7 @@ class App extends Component {
     return <div>
       <Switch>
         <Route exact component={HomePage} path="/"></Route>
-        <Route component={HatsPage} path="/hats"></Route>
+        <Route component={ShopPage} path="/shop"></Route>
       </Switch>
 
     </div>;
