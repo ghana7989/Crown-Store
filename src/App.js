c9a1b14f77fdb105f8ec102bd279138d78097f59
ghana7989 <ghana.chindukuri@gmail.com>
2020-08-06 17:00:37 +0530

    Adding Header Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/Homepage/homepage.components';
 import ShopPage from './pages/shop/shop.components.jsx'
+import Header from './components/header-component/header.component';
 
 
 
@@ -11,6 +12,7 @@ import ShopPage from './pages/shop/shop.components.jsx'
 class App extends Component {
   render() {
     return <div>
+      <Header></Header>
       <Switch>
         <Route exact component={HomePage} path="/"></Route>
         <Route component={ShopPage} path="/shop"></Route>
