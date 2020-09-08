8b9a2f6d59115c86a03ec795f5221cf344e1fa3d
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-08 16:29:28 +0530

    Updates

---
+++ precache-manifest.733025bd70edd3d845c59d739fb4afc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c39c4f484111be16ab8cca22b32abac",
+    "url": "/Crown-Store/index.html"
+  },
+  {
+    "revision": "475dc027137b0135f1fc",
+    "url": "/Crown-Store/static/css/main.a54cebda.chunk.css"
+  },
+  {
+    "revision": "0d48e2ffdddf265ea580",
+    "url": "/Crown-Store/static/js/2.4b856b02.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Crown-Store/static/js/2.4b856b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475dc027137b0135f1fc",
+    "url": "/Crown-Store/static/js/main.5e0f68a9.chunk.js"
+  },
+  {
+    "revision": "8d883f75783aeaf02f26",
+    "url": "/Crown-Store/static/js/runtime-main.797ea380.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Crown-Store/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Crown-Store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7f501aec9e69624a005bba81c01fb4b.js"
+  "/Crown-Store/precache-manifest.733025bd70edd3d845c59d739fb4afc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crown-Store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
